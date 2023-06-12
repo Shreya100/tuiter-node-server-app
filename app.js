e8431155160735554ec850c66ea69d54054a5645
Shreya100 <shreya.alla21@gmail.com>
2023-06-12 12:08:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import session from "express-session";
+
+const app = express();
+
+app.use(
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
+app.use(express.json());
+TuitsController(app)
+HelloController(app)
+UserController(app)
+AuthController(app);
+
+// Handle CORS preflight requests
+app.options("*", cors());
+
+app.listen(process.env.PORT || 4000);
+
+
