114fe3107191ff75ebadb7794025d0ea22329090
Shreya100 <shreya.alla21@gmail.com>
2023-06-20 12:58:40 -0400

    a6 Mongo su23

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -1,53 +1,54 @@
 import * as usersDao from "./users-dao.js";
+import people from "./users.js";
+let users = people;
 
-
-const AuthController = (app) => {
-    const register = (req, res) => { 
-    const username = req.body.username;
-    const user = usersDao.findUserByUsername(username);
-    if (user) {
-        res.sendStatus(409);
-        return;
-    }
-   const newUser = usersDao.createUser(req.body);
-   req.session["currentUser"] = newUser;
-   res.json(newUser);
-
- };
- const login    = (req, res) => { 
+function AuthenticationController(app) {
+  const login = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const user = usersDao.findUserByCredentials(username, password);
-    if (user) {
+    if (username && password) {
+      const user = await usersDao.findUserByCredentials(username, password);
+      if (user) {
         req.session["currentUser"] = user;
         res.json(user);
+      } else {
+        res.sendStatus(403);
+      }
     } else {
-        res.sendStatus(404);
+      res.sendStatus(403);
     }
-
- };
- const profile  = (req, res) => { 
+  };
+  const register = async (req, res) => {
+    // const user = users.find((user) => user.username === req.body.username);
+    const user = await usersDao.findUserByUsername(req.body.username);
+    if (user) {
+      res.sendStatus(403);
+      return;
+    }
+    const newUser = await usersDao.createUser(req.body);
+    req.session["currentUser"] = newUser;
+    res.json(newUser);
+  };
+  const profile = (req, res) => {
     const currentUser = req.session["currentUser"];
-    if (!currentUser) {
-        res.sendStatus(404);
-        return;
+    if (currentUser) {
+      res.json(currentUser);
+    } else {
+      res.sendStatus(403);
     }
-    res.json(currentUser);
-
- };
- const logout   = (req, res) => { 
+  };
+  const logout = (req, res) => {
     req.session.destroy();
     res.sendStatus(200);
+  };
 
- };
- const update   = (req, res) => { };
+  const update   = async(req, res) => { };
 
+  app.post("/api/users/register", register);
+  app.post("/api/users/login",    login);
+  app.post("/api/users/profile",  profile);
+  app.post("/api/users/logout",   logout);
+  app.put ("/api/users",          update);
+}
 
- app.post("/api/users/register", register);
- app.post("/api/users/login",    login);
- app.post("/api/users/profile",  profile);
- app.post("/api/users/logout",   logout);
- app.put ("/api/users",          update);
-};
-export default AuthController;
-
+export default AuthenticationController;
